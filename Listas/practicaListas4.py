ba80296cc32ffe8c02bfc848054c6ef8ebdd5990
mottasb <159779317+mottasb@users.noreply.github.com>
2024-02-12 07:35:32 -0500

    Add files via upload

---
+++ Listas/practicaListas4.py
@@ -0,0 +1,9 @@
+List1 = ["manzana","carro","cereza"]
+List2 = [1,2,3,4,5,6,7,8,9]
+List3 = [False, True, True,False]
+print(List1) \n
+print(len(List1)) \n
+print(List2) \n
+print(len(List2)) \n
+print(List3) \n
+print(len(List3)) \n 
